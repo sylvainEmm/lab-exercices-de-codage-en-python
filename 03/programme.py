b5fbd0c493ed397984e10755d1499e0b2543d19a
sylvain <sylvainmakak@gmail.com>
2020-12-09 11:47:46 -0500

    ajout de la fonction exo_03 inversant date

--- 03/programme.py
+++ 03/programme.py
@@ -7,7 +7,8 @@
 def exo_03(tuple):
   # Ajouter le code permettant d'inverser la tuple
   # sous la forme 'Decembre' 9 2020
-  ??? 
+  x=("decembre",9,2020)
+  b,a,c=x
   return (b, a , c)
 
 def main():
