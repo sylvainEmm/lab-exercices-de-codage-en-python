4bb9011bcb72d9abd88e309fb259a4b2cbd66d96
Brice <62551735+b300098957@users.noreply.github.com>
2020-12-09 03:07:31 -0500

    Update programme.py

--- 03/programme.py
+++ 03/programme.py
@@ -6,12 +6,12 @@
 
 def exo_03(tuple):
   # Ajouter le code permettant d'inverser la tuple
-  # sous la forme Decembre 9 2020
+  # sous la forme 'Decembre' 9 2020
   ??? 
   return (b, a , c)
 
 def main():
-  print( exo_03( ( 9, "Decembre" , 2020 ) ) )
+  print( exo_03( ( 9, 'Decembre' , 2020 ) ) )
 
 if __name__== "__main__":
   main()
